9b2629aa607f86f2e489c536b3b789e283ca0f3e
ares <arkeiiuc3@gmail.com>
2025-07-04 05:54:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelectorAll(".a-navbar");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
